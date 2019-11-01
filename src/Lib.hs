db2b2b7599da4ccac0a9a4d8db5e586232339a3e
Kirill Lastovirya <kirill.lastovirya@jobseeker.com.au>
2019-11-01 17:11:20 +1100

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,40 @@
+module Lib
+    ( mainFunc
+    ) where
+
+import Graphics.Gloss
+import Data.Word
+import Data.ByteString (ByteString, pack)
+
+-----------------------------------------
+-- conversions
+
+screenWidth = 600
+screenHeight = 600
+-- positionX = - 1.0
+-- positionY = - 1.0
+-- worldWidth = 2.0
+-- worldHeight = 2.0
+
+-----------------------------------------
+-- fractal
+
+colorFor :: (Int, Int) -> [Word8]
+colorFor coords = case coords of
+  (x, y) | rem x 10 == 0 || rem y 10 == 0-> [0, 128, 0, 100]
+  otherwise -> [0, 0, 0, 100]
+
+allPixels :: Int -> Int -> [(Int,Int)]
+allPixels width height = [(i, j) | i <- [0..width-1], j <- [0..height-1]]
+
+---------------------------------------
+--- IO related part
+
+bitmapData :: ByteString
+bitmapData = pack $ concat (map colorFor (allPixels screenWidth screenHeight))
+
+ourPicture :: Picture
+ourPicture = bitmapOfByteString screenWidth screenHeight (BitmapFormat TopToBottom PxRGBA) bitmapData True
+
+mainFunc :: IO ()
+mainFunc = display (InWindow "Nice Window" (screenWidth, screenHeight) (10, 10)) white ourPicture
